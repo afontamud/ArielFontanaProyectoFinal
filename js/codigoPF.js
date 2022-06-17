41af53ffc34bcc207f42f8c7d64613abf942516f
afontamud <arielfontaph@gmail.com>
2022-06-17 11:47:01 -0300

    se agrega

--- js/codigoPF.js
+++ js/codigoPF.js
@@ -1,15 +1,48 @@
-let producto = [];
-let total = 0;
-alert ("Aprovecha nuestros descuentos de estudiantes")
-let nombreUsuario = prompt("Ingresa tu nombre")
-let inicio = confirm( nombreUsuario + " Usted acepta terminos y condiciones")
 
-function add(producto, precio, ) {
-    console.log (producto, precio);
-    total = total + precio;
-    document.getElementById("comprar").innerHTML = `Pagar $${total}`;
+let carrito=[];
+if(localStorage.getItem("carrito")!=null){
+    carrito=JSON.parse(localStorage.getItem("carrito"));
 }
+let lista=document.getElementById("milista");
 
-function pay(){
-    windows.alert(producto.join(", \n"));
+//renderizar
+renderizarProductos();
+
+function renderizarProductos(){
+    for (const producto of productos){
+        lista.innerHTML+=`<li class="col-sm-3 list-group-item">
+        <img src=${producto.foto} width="250" height="250">
+        <p> Producto: ${producto.nombre}</p>
+        <p><strong> $ ${producto.precio} </strong></p>
+        <button class='btn btn-info' id='btn${producto.id}'>Comprar</button>
+        </li>`;
+    }
+
+    productos.forEach(producto => {
+        document.getElementById(`btn${producto.id}`).addEventListener("click", function(){
+            agregarAlCarrito(producto);
+        });
+    });
+} 
+
+function agregarAlCarrito(productoNuevo){
+    carrito.push(productoNuevo);
+    console.log(carrito);
+    Toastify({
+        text: "Producto: "+productoNuevo.nombre+" "+"agregado al carro",
+        className: "info",
+        style: {
+          background: "linear-gradient(to right,#24c78f)",
+        }
+      }).showToast();
+
+    //alert("Producto: "+productoNuevo.nombre+" "+"agregado al carro");
+    document.getElementById("tablabody").innerHTML+=`
+        <tr>
+            <td>${productoNuevo.id}</td>
+            <td>${productoNuevo.nombre}</td>
+            <td>${productoNuevo.precio}</td>
+        </tr>
+    `;
+    localStorage.setItem("carrito",JSON.stringify (carrito));
 }
